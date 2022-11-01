e9004771066cf39a57a06c81430452de21cb6114
MMbay64 <115948389+MMbay64@users.noreply.github.com>
2022-11-01 00:39:59 -0400

    New Achievements ( w.i.p )

---
+++ Source/better Achievements.hx
@@ -0,0 +1,12 @@
+Package;
+
+Class Achievements [
+public static var AchievementsStuff:Array<dynamic> - [ //lil psych engine stuffy
+                                  ["Can I get the approval?","beat week 1 with no misses",  false],
+                                  ["A little treat!","beat week 2 with no misses",          false],
+                                  ["YOOOO YOOOO YOOOO!","beat week 3 with no misses",       false],
+                                  ["Slay...","watch the dancing henchmen die 100,000 times",false],
+                                  ["SANTA GOT SHOT?","beat week 5 with no misses",          false],
+                                  ["highschool got mad hot","beat week 6 with no misses",   false],
+                                  ["You just wanna make me cry","beat week 7 with no misses",true]
+        ];
